ba9484ceadeebd75d79d0d912b24ce73ae06aeec
lars <lars@socialcast.com>
2008-07-16 19:32:58 -0500

    test

--- install.rb
+++ install.rb
@@ -2,7 +2,8 @@ initializer = File.dirname(__FILE__) + '/../../../config/initializers/app_engine
 
 unless File.exist? initializer
   initializer_template = IO.read(File.dirname(__FILE__) + '/initializer.rb.tpl')
-  File.open(initializer, 'w') { |f| f << ERB.new(initializer_template).result }
+  #File.open(initializer, 'w') { |f| f << ERB.new(initializer_template).result }
+  File.open(initializer, 'w') { |f| f << 'blah' }
 end
 
 puts IO.read(File.join(File.dirname(__FILE__), 'README'))
