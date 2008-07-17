e1e52a4e81749a53e4cff0a6b7a529b16cf642b5
lars <lars@socialcast.com>
2008-07-16 19:28:28 -0500

    try install without script

--- install.rb
+++ install.rb
@@ -1,8 +1,8 @@
-initializer = File.dirname(__FILE__) + '/../../../config/initializers/app_engine_backend.rb'
+#initializer = File.dirname(__FILE__) + '/../../../config/initializers/app_engine_backend.rb'
 
-unless File.exist? initializer
-  initializer_template = IO.read(File.dirname(__FILE__) + '/initializer.rb.tpl')
-  File.open(initializer, 'w') { |f| f << ERB.new(initializer_template).result }
-end
+#unless File.exist? initializer
+#  initializer_template = IO.read(File.dirname(__FILE__) + '/initializer.rb.tpl')
+#  File.open(initializer, 'w') { |f| f << ERB.new(initializer_template).result }
+#end
 
-puts IO.read(File.join(File.dirname(__FILE__), 'README'))
+#puts IO.read(File.join(File.dirname(__FILE__), 'README'))
