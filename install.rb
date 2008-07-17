9f5367f0c4046eef06dc9bba2a358ba0a8730939
lars <lars@socialcast.com>
2008-07-16 19:31:26 -0500

    test

--- install.rb
+++ install.rb
@@ -1,7 +1,7 @@
 initializer = File.dirname(__FILE__) + '/../../../config/initializers/app_engine_backend.rb'
 
 unless File.exist? initializer
-#  initializer_template = IO.read(File.dirname(__FILE__) + '/initializer.rb.tpl')
+  initializer_template = IO.read(File.dirname(__FILE__) + '/initializer.rb.tpl')
 #  File.open(initializer, 'w') { |f| f << ERB.new(initializer_template).result }
 end
 
